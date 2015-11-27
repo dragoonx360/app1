d6f96c0e11433676a052b6485e4de7a4840e4e34
Dragoonx <dragoonx360@gmail.com>
2015-11-28 00:44:11 +0100

    Sube las vistas creadas en la app Movil

--- misitio/app1/views.py
+++ misitio/app1/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Movil
 
 def post_list(request):
-        return render(request, 'app1/post_list.html', {})
+    moviles = Movil.objects.filter(fecha_lanzamiento__lte=timezone.now()).order_by('fecha_lanzamiento')
+    return render(request, 'app1/post_list.html', {'moviles': moviles})
